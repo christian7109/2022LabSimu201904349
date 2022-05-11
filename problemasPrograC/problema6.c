183be37abf3d4ebfc8aa99bf675acb5f59968189
christian <aju17b@gmail.com>
2022-05-11 14:54:29 -0600

    el problema 6

--- problemasPrograC/problema6.c
+++ problemasPrograC/problema6.c
@@ -9,5 +9,40 @@ resumen:
 
 //librerias
 #include <stdio.h>
+int nV, nSum;
+int cont=0;
+float sumatoria=0;
+float A;
+int main(){
+    puts("Ingrese el número de vertices de su poligono (>3)");
+    scanf("%d", &nV);
+    int matrizXY[nV][2];
+if (nV>=3)
+{
+    for (cont = 0; cont < nV; cont++)
+    {
+        puts("Ingrese las coordenada x");
+        scanf("%d \n", &matrizXY[cont][0]);
+        puts("Ingrese las coordenada y");
+        scanf("%d \n", &matrizXY[cont][1]);
+    }
+    printf("\n");
+    //imprimimos la matriz
+    puts("Tu poligono tiene estás vertices:");
+    for (cont = 0; cont < nV; cont++)
+    {
+        printf("(%d, %d) \n", matrizXY[cont][0],matrizXY[cont][1] );
+    }
+    //hacemos el n de la sumatoria
+    nSum=nV-1;
+    for (int i = 0; i < nSum; i++)
+    {
+        sumatoria +=(matrizXY[i][0]*matrizXY[i+1][1]) - (matrizXY[i+1][0]*matrizXY[i][1]);
+    }
+    A +=(matrizXY[nSum][0]*matrizXY[0][1])-(matrizXY[0][0]*matrizXY[nSum][1]);
+    A=A/2;
+    printf("El área de tu poligono es: %0.2f \n" , A);
+}
 
+}
 
