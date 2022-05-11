c24ae489bdc7ae9bee50cec8270bafc796f76a15
christian <aju17b@gmail.com>
2022-05-11 14:56:01 -0600

    p6

--- problemasPrograC/problema6.c
+++ problemasPrograC/problema6.c
@@ -14,14 +14,14 @@ int cont=0;
 float sumatoria=0;
 float A;
 int main(){
-    puts("Ingrese el número de vertices de su poligono (>3)");
+    puts("Ingrese el número de vertices de su poligono (>3)"); //pedimos los vertices
     scanf("%d", &nV);
-    int matrizXY[nV][2];
+    int matrizXY[nV][2]; //definimos una matriz 
 if (nV>=3)
 {
     for (cont = 0; cont < nV; cont++)
     {
-        puts("Ingrese las coordenada x");
+        puts("Ingrese las coordenada x");   //obtenemos las coordenadas de los vertices
         scanf("%d \n", &matrizXY[cont][0]);
         puts("Ingrese las coordenada y");
         scanf("%d \n", &matrizXY[cont][1]);
@@ -31,17 +31,17 @@ if (nV>=3)
     puts("Tu poligono tiene estás vertices:");
     for (cont = 0; cont < nV; cont++)
     {
-        printf("(%d, %d) \n", matrizXY[cont][0],matrizXY[cont][1] );
+        printf("(%d, %d) \n", matrizXY[cont][0],matrizXY[cont][1] ); //imprimimos las coordenadas 
     }
     //hacemos el n de la sumatoria
     nSum=nV-1;
-    for (int i = 0; i < nSum; i++)
+    for (int i = 0; i < nSum; i++)  //hacemos las sumatorias
     {
         sumatoria +=(matrizXY[i][0]*matrizXY[i+1][1]) - (matrizXY[i+1][0]*matrizXY[i][1]);
     }
     A +=(matrizXY[nSum][0]*matrizXY[0][1])-(matrizXY[0][0]*matrizXY[nSum][1]);
     A=A/2;
-    printf("El área de tu poligono es: %0.2f \n" , A);
+    printf("El área de tu poligono es: %0.2f \n" , A); //imprimimos
 }
 
 }
