645a6e67e1937913695cb9c8482025c894026898
christian <aju17b@gmail.com>
2022-05-16 14:21:03 -0600

    que sad

--- EXAMENFINAL/problema2.cpp
+++ EXAMENFINAL/problema2.cpp
@@ -8,4 +8,32 @@ Resumen:
 */
 
 //librerias
-#include <iostream>
+#include <iostream>     //poder tener acceso a los dispositivos estándar de entrada y/o salida.
+using namespace std;    //es el de dar acceso al espacio de nombres (namespace) std, donde se encuentra encerrada toda la librería estándar.
+
+class Cohete
+{
+    public:
+        // Aqui agregamos los publicos
+        double E_0, TSFC;
+    public:
+        Cohete(double A, double B, double C, double D, double F, double G)
+        { 
+            E_0=A;
+            TSFC=B;   
+        }
+};
+
+int main() {
+    Cohete cohete1(3E7, 3.248E-4, 61.27, 201.06, 1.1E5, 1.5E6);
+    Cohete cohete2(2.7E7,2.248E-4, 61.27,201.06,1.1E5,1.5E6);
+    Cohete cohete3(2.5E7, 2.248E-4,70.25, 215,1.8E5, 2.1E6);
+    cout << "Para el cohete 1 son: \n";
+    cohete1.Unico();
+    cout << "Para el cohete 2 son: \n";
+    cohete2.Unico();
+    cout << "Para el cohete 3 son: \n";
+    cohete3.Unico();
+    cout << endl;
+    return 0;
+}
--- EXAMENFINAL/problemaUno.c
+++ EXAMENFINAL/problemaUno.c
@@ -20,43 +20,43 @@ float g_0=9.81;
 float T_0=288.15;
 float P_0=101325;
 float y=0.49;
-float densidadAire();
+float densidadAire(); //prototipamos funciones en las cuales se ve que hay muchas variables.
 float friccionatm(float condicionCD, float seccionTA);
 float gdeY();
-float CombustibleF(float ts_i,float E_i0);
+float Consumo(float ts_i,float E_i0);
 float dfCombus(float consumoEm, float empuje_0);
 float masaTotalCombustible(float masaCombusInicial,float Mpropulsor, float ts_i,float E_i0);
 
 int main(){
-    float cohete1[]={3E7, 3.248E-4, 61.27, 201.06, 1.1E5, 1.5E6};
+    float cohete1[]={3E7, 3.248E-4, 61.27, 201.06, 1.1E5, 1.5E6}; //guardamos los valores constantes especificos para cada cohete
     float cohete2[]={2.7E7,2.248E-4, 61.27,201.06,1.1E5,1.5E6};
     float cohete3[]={2.5E7, 2.248E-4,70.25, 215,1.8E5, 2.1E6};
-    CombustibleF(3.248E-4,3E7);
+    Consumo(3.248E-4,3E7);
 
 }
 
 
-float densidadAire( ){
+float densidadAire( ){  // se desarrolla la ecuación que nos encuentra la densidad del aire 
     float rho=0;
-    rho=(P_0/(R*T_0)* pow((1-((L*y)/T_0)),(g_0/(R*L))));
+    rho=(P_0/(R*T_0)* pow((1-((L*y)/T_0)),(g_0/(R*L)))); //este sería el resultado 
     return rho;
 }
-float friccionatm(float condicionCD, float seccionTA){
+float friccionatm(float condicionCD, float seccionTA){  //aquí se obtiene la friccion de la atmosfera, la cual depende de la velocidad del cohete
     float F_a=0;
     F_a=0.5*(densidadAire()*condicionCD*seccionTA );
 }
 float gdeY(){
-    float g=(G*Mt)/pow((Rt+y),2);
+    float g=(G*Mt)/pow((Rt+y),2); //en esta funcion se obtiene la gravedad que depende de la altura
     return g;
 }
 
-float masaTotalCombustible(float masaCombusInicial,float Mpropulsor, float ts_i,float E_i0 ){
-    float masatotalC=masaCombusInicial;
-    masatotalC= Mpropulsor * CombustibleF(ts_i,E_i0);
+float masaTotalCombustible(float masaCombusInicial,float Mpropulsor, float ts_i,float E_i0 ){   //esta funcion nos muetra la masa  
+    float masatotalC=masaCombusInicial;     //total en la cual está en funcion de otra función llamada combustible
+    masatotalC= Mpropulsor * Consumo(ts_i,E_i0);
     return masatotalC;
 }
 
-float CombustibleF(float ts_i,float E_i0){
+float Consumo(float ts_i,float E_i0){ //en esa funcion se busco encontrar la perdida de combustible con respecto del tiempo 
     float deltah=0.1;
     float t=0;
     float masaCombust;
@@ -69,8 +69,8 @@ float CombustibleF(float ts_i,float E_i0){
     
 
 }
-float dfCombus(float consumoEm, float empuje_0){
-    float resp;
+float dfCombus(float consumoEm, float empuje_0){ //en esta función tenemos la derivada de la masa en la que varia el combustible 
+    float resp; 
     resp=-consumoEm*empuje_0;
     return resp;
 }
